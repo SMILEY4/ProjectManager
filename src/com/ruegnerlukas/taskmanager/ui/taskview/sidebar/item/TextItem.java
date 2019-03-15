f44b1a28f00a55f3e6e9fe7c422ceb609449caf8
Lukas Ruegner <ruegnerlukas@gmail.com>
2019-03-15 21:34:33 +0100

    Sidebar: TextItem uses TextField for Single-Line-TextAttribs

--- src/com/ruegnerlukas/taskmanager/ui/taskview/sidebar/item/TextItem.java
+++ src/com/ruegnerlukas/taskmanager/ui/taskview/sidebar/item/TextItem.java
@@ -13,12 +13,14 @@ import com.ruegnerlukas.taskmanager.logic.Logic;
 import com.ruegnerlukas.taskmanager.ui.taskview.sidebar.Sidebar;
 import javafx.scene.Parent;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class TextItem extends SidebarItem {
 
 
 	private double fieldHeight;
 	private TextArea textArea;
+	private TextField textField;
 
 
 
@@ -41,7 +43,11 @@ public class TextItem extends SidebarItem {
 
 	@Override
 	protected double getFieldHeight() {
-		return fieldHeight + 6;
+		if (textArea != null) {
+			return fieldHeight + 6;
+		} else {
+			return 32;
+		}
 	}
 
 
@@ -51,34 +57,71 @@ public class TextItem extends SidebarItem {
 	protected Parent createValueField(Task task, TaskAttribute attribute) {
 
 		TextAttributeData data = (TextAttributeData) attribute.data;
-		this.fieldHeight = Math.max(33, 21.3 * (data.multiline ? data.nLinesExpected : 1) + 9);
-		textArea = new TextArea();
-		textArea.setMinSize(0, 33);
-		textArea.setPrefSize(10000, fieldHeight);
-		textArea.setMaxSize(10000, fieldHeight);
 
-		this.setPrefSize(10000, -1);
-		this.setMaxSize(10000, 10000);
+		if (data.multiline) {
 
-		Logic.tasks.getAttributeValue(task, attribute.name, new Request<TaskAttributeValue>(true) {
-			@Override
-			public void onResponse(Response<TaskAttributeValue> response) {
-				if (response.getValue() instanceof NoValue) {
-					textArea.setText("");
-				} else {
-					textArea.setText(((TextValue) response.getValue()).getText());
+			this.textField = null;
+
+			this.fieldHeight = Math.max(33, 21.3 * (data.multiline ? data.nLinesExpected : 1) + 9);
+			textArea = new TextArea();
+			textArea.setMinSize(0, 33);
+			textArea.setPrefSize(10000, fieldHeight);
+			textArea.setMaxSize(10000, fieldHeight);
+
+			this.setPrefSize(10000, -1);
+			this.setMaxSize(10000, 10000);
+
+			Logic.tasks.getAttributeValue(task, attribute.name, new Request<TaskAttributeValue>(true) {
+				@Override
+				public void onResponse(Response<TaskAttributeValue> response) {
+					if (response.getValue() instanceof NoValue) {
+						textArea.setText("");
+					} else {
+						textArea.setText(((TextValue) response.getValue()).getText());
+					}
 				}
-			}
-		});
+			});
 
-		textArea.focusedProperty().addListener((observable, oldValue, newValue) -> {
-			if (!newValue) {
-				Logic.tasks.setAttributeValue(task, attribute, new TextValue(textArea.getText()));
-			}
+			textArea.focusedProperty().addListener((observable, oldValue, newValue) -> {
+				if (!newValue) {
+					Logic.tasks.setAttributeValue(task, attribute, new TextValue(textArea.getText()));
+				}
 
-		});
+			});
 
-		return textArea;
+			return textArea;
+
+		} else {
+
+			this.textArea = null;
+
+			textField = new TextField();
+			textField.setMinSize(0, 32);
+			textField.setPrefSize(10000, 32);
+			textField.setMaxSize(10000, 32);
+
+			Logic.tasks.getAttributeValue(task, attribute.name, new Request<TaskAttributeValue>(true) {
+				@Override
+				public void onResponse(Response<TaskAttributeValue> response) {
+					if (response.getValue() instanceof NoValue) {
+						textField.setText("");
+					} else {
+						textField.setText(((TextValue) response.getValue()).getText());
+					}
+				}
+			});
+
+			textField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+				if (!newValue) {
+					Logic.tasks.setAttributeValue(task, attribute, new TextValue(textField.getText()));
+				}
+
+			});
+
+			return textField;
+
+		}
+
 	}
 
 
@@ -86,7 +129,11 @@ public class TextItem extends SidebarItem {
 
 	@Override
 	protected TaskAttributeValue getValue() {
-		return new TextValue(textArea.getText());
+		if (textArea != null) {
+			return new TextValue(textArea.getText());
+		} else {
+			return new TextValue(textField.getText());
+		}
 	}
 
 
