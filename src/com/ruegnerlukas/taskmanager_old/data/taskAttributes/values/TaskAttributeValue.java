9a8cfe4aa942460f30f8826bca26ddd7131bf83d
Lukas Ruegner <ruegnerlukas@gmail.com>
2019-06-01 12:07:56 +0200

    Dropped old files

--- src/com/ruegnerlukas/taskmanager_old/data/taskAttributes/values/TaskAttributeValue.java
+++
@@ -1,7 +0,0 @@
-package com.ruegnerlukas.taskmanager_old.data.taskAttributes.values;
-
-public interface TaskAttributeValue extends Comparable<TaskAttributeValue> {
-
-	public Object getValue();
-
-}
--- src/com/ruegnerlukas/taskmanager_old/utils/uielements/dialogaddtag/DialogAddTag.java
+++
@@ -1,6 +0,0 @@
-package com.ruegnerlukas.taskmanager_old.utils.uielements.dialogaddtag;
-
-public class DialogAddTag {
-
-	
-}
